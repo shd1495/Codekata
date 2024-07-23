a4cb93ceff6268dfda09c7e23f7039c620cf50fe
박지영 <124862122+shd1495@users.noreply.github.com>
2024-07-23 21:47:15 +0900

    정수 내림차순으로 배치하기
    문제 설명
    
    함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
    
    제한 조건
    
    n은 1이상 8000000000 이하인 자연수입니다.

--- Programmers/js/pb01.js
+++ Programmers/js/pb01.js
@@ -1,1 +1,9 @@
-
+function solution(n) {
+    let nArray = Array.from(n.toString());
+    
+    nArray = nArray.sort((a,b)=>(b-a)).map(Number);
+    
+    let answer = parseInt(nArray.join(''));
+    
+    return answer;
+}

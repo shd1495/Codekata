4328b69c742962f0d84d51789b83c3619260c639
박지영 <124862122+shd1495@users.noreply.github.com>
2024-08-11 20:11:13 +0900

    모의고사
    문제 설명
    
    수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
    
    1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
    2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
    3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
    
    1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
    
    제한 조건
    
    시험은 최대 10,000 문제로 구성되어있습니다.
    문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
    가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.

---
+++ Programmers/js/pb38.js
@@ -0,0 +1,34 @@
+function solution(answers) {
+    let answer = [];
+    let answer1 = [1, 2, 3, 4, 5];
+    let answer2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    let answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let temp1 = 0;
+    let temp2 = 0;
+    let temp3 = 0;
+    for (let i = 0; i < answers.length; i++) {
+        if (answer1.length < answers.length) {
+            answer1 = answer1.concat(answer1);
+        }
+        if (answer2.length < answers.length) {
+            answer2 = answer2.concat(answer2);
+        }
+        if (answer3.length < answers.length) {
+            answer3 = answer3.concat(answer3);
+        }
+        
+        if (answers[i] == answer1[i]) temp1++;
+        if (answers[i] == answer2[i]) temp2++;
+        if (answers[i] == answer3[i]) temp3++;
+    }
+    if (temp1 >= temp2 && temp1 >= temp3) {
+        answer.push(1);
+    }
+    if (temp2 >= temp1 && temp2 >= temp3) {
+        answer.push(2);
+    }
+    if (temp3 >= temp1 && temp3 >= temp2) {
+        answer.push(3);
+    }
+    return answer;
+}
